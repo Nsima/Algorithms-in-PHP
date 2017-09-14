832423f578e65f5af1fbce6330c39f8ab0309434
Knowvees <georgensima@gmail.com>
2017-09-14 10:50:13 +0100

    LastInstance.php

---
+++ LastInstance.php
@@ -0,0 +1,23 @@
+<?php
+/**
+A function that finds the last instance of a character in a string
+
+There are a lot of ways to solve this though, this is my own method.
+Most of this Algorithm issues are gotten from HackerRank and TopCoder 
+**/
+
+function lastchar($string, $char){
+
+$str = NULL;
+
+//traverse the entire string
+
+  while($string++ != NULL){
+    if($string==ch){
+      $str = $string;
+    }
+  }
+  return $str;
+}
+
+?>
